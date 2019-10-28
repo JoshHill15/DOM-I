69c3a8b549151343e997dcaa892a53ece0cfbfc1
josh Hill <Josh_hill15@me.com>
2019-10-28 17:02:56 -0400

    fixes

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM <br/>Is <br/> Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -65,7 +65,7 @@ let button = document.querySelector("button");
 button.textContent = siteContent["cta"]["button"];
 
 let heading = document.querySelector(".cta-text h1");
-heading.textContent = siteContent["cta"]["h1"]
+heading.innerHTML = siteContent["cta"]["h1"]
 
 let miniHeaders = document.querySelectorAll(".main-content h4");
 console.log(miniHeaders)
